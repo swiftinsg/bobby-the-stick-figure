9f083899e0f52f025856f21895b5006e09543217
YJ Soon <yjsoon@tinkertanker.com>
2025-08-02 06:41:43 +0800

    Initial Commit

---
+++ stick-figure/stick_figureApp.swift
@@ -0,0 +1,17 @@
+//
+//  stick_figureApp.swift
+//  stick-figure
+//
+//  Created by YJ Soon on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct stick_figureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
